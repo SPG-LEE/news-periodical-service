5100875f0b1b89b7f6456885a556e59e45163cf0
mrlijianlong <517989745@qq.com>
2018-05-20 19:18:14 +0800

    解决中文乱码问题

--- src/main/java/sq/news/periodical/entity/Article.java
+++ src/main/java/sq/news/periodical/entity/Article.java
@@ -10,18 +10,16 @@ import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import sq.base.BaseEntity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 /**
  * 文章
- *
- *
+ * <p>
+ * <p>
  * {name:"",loginName:"",lastLoginDate:""}
  */
 @ApiModel(value = "文章")
@@ -32,161 +30,187 @@ import java.util.List;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
 public class Article extends BaseEntity {
 
-	private static final long serialVersionUID = 1L;
-	public static final String NAME = "文章";
-	@ApiModelProperty(value = "文章标题")
-	@JsonProperty
-	private String title;
-	@ApiModelProperty(value = "文章摘要")
-	@JsonProperty
-	private String description;
-	@ApiModelProperty(value = "作者名")
-	@JsonProperty
-	private String author;
-	@ApiModelProperty(value = "作者id")
-	@JsonProperty
-	private long authorId;
-	@ApiModelProperty(value = "发布日期")
-	@JsonProperty
-	private Date publishDate;
-	@ApiModelProperty(value = "期刊Id")
-	@JsonProperty
-	private long periodicalId;
-	@ApiModelProperty(value = "期刊标题")
-	@JsonProperty
-	private String periodical;
-	@ApiModelProperty(value = "版次Id")
-	@JsonProperty
-	private long editionId;
-	@ApiModelProperty(value = "正文")
-	@JsonProperty
-	private String content;
-	@ApiModelProperty(value = "版次名")
-	@JsonProperty
-	private String editionPeriod;
-	@ApiModelProperty(value = "封面图片")
-	@JsonProperty
-	private String mainImage;
-	@ApiModelProperty(value = "是否审核")
-	@JsonProperty
-	private boolean hasAudit;
-	@ApiModelProperty(value = "是否显示")
-	@JsonProperty
-	private boolean hasShow;
-	@ApiModelProperty(value = "评论")
-	@JsonProperty
-	private List<ArticleComment> comments = new ArrayList<>();
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "文章";
+    @ApiModelProperty(value = "文章标题")
+    @JsonProperty
+    private String title;
+    @ApiModelProperty(value = "文章摘要")
+    @JsonProperty
+    private String description;
+    @ApiModelProperty(value = "作者名")
+    @JsonProperty
+    private String author;
+    @ApiModelProperty(value = "作者id")
+    @JsonProperty
+    private long authorId;
+    @ApiModelProperty(value = "发布日期")
+    @JsonProperty
+    private Date publishDate;
+    @ApiModelProperty(value = "期刊Id")
+    @JsonProperty
+    private long periodicalId;
+    @ApiModelProperty(value = "期刊标题")
+    @JsonProperty
+    private String periodical;
+    @ApiModelProperty(value = "版次Id")
+    @JsonProperty
+    private long editionId;
+    @ApiModelProperty(value = "正文")
+    @JsonProperty
+    private String content;
 
-	public boolean isHasAudit() {
-		return hasAudit;
-	}
+    private byte[] contentByte;
+    @ApiModelProperty(value = "版次名")
+    @JsonProperty
+    private String editionPeriod;
+    @ApiModelProperty(value = "封面图片")
+    @JsonProperty
+    private String mainImage;
+    @ApiModelProperty(value = "是否审核")
+    @JsonProperty
+    private boolean hasAudit;
+    @ApiModelProperty(value = "是否显示")
+    @JsonProperty
+    private boolean hasShow;
+    @ApiModelProperty(value = "评论")
+    @JsonProperty
+    private List<ArticleComment> comments = new ArrayList<>();
 
-	public void setHasAudit(boolean hasAudit) {
-		this.hasAudit = hasAudit;
-	}
+    public boolean isHasAudit() {
+        return hasAudit;
+    }
 
-	public boolean isHasShow() {
-		return hasShow;
-	}
+    public void setHasAudit(boolean hasAudit) {
+        this.hasAudit = hasAudit;
+    }
 
-	public void setHasShow(boolean hasShow) {
-		this.hasShow = hasShow;
-	}
+    public boolean isHasShow() {
+        return hasShow;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public void setHasShow(boolean hasShow) {
+        this.hasShow = hasShow;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String getAuthor() {
-		return author;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setAuthor(String author) {
-		this.author = author;
-	}
+    public String getAuthor() {
+        return author;
+    }
 
-	public long getAuthorId() {
-		return authorId;
-	}
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 
-	public void setAuthorId(long authorId) {
-		this.authorId = authorId;
-	}
+    public long getAuthorId() {
+        return authorId;
+    }
 
-	public Date getPublishDate() {
-		return publishDate;
-	}
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
 
-	public void setPublishDate(Date publishDate) {
-		this.publishDate = publishDate;
-	}
+    public Date getPublishDate() {
+        return publishDate;
+    }
 
-	public long getPeriodicalId() {
-		return periodicalId;
-	}
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
 
-	public void setPeriodicalId(long periodicalId) {
-		this.periodicalId = periodicalId;
-	}
+    public long getPeriodicalId() {
+        return periodicalId;
+    }
 
-	public long getEditionId() {
-		return editionId;
-	}
+    public void setPeriodicalId(long periodicalId) {
+        this.periodicalId = periodicalId;
+    }
 
-	public void setEditionId(long editionId) {
-		this.editionId = editionId;
-	}
-	@Column(columnDefinition="TEXT")
-	public String getContent() {
-		return content;
-	}
+    public long getEditionId() {
+        return editionId;
+    }
 
-	public void setContent(String content) {
-		this.content = content;
-	}
+    public void setEditionId(long editionId) {
+        this.editionId = editionId;
+    }
 
-	public String getEditionPeriod() {
-		return editionPeriod;
-	}
+    @Column(columnDefinition = "BLOB")
+    public byte[] getContentByte() {
+        return contentByte;
+    }
 
-	public void setEditionPeriod(String editionPeriod) {
-		this.editionPeriod = editionPeriod;
-	}
-
-	public String getMainImage() {
-		return mainImage;
-	}
-
-	public void setMainImage(String mainImage) {
-		this.mainImage = mainImage;
-	}
+    public void setContentByte(byte[] contentByte) {
+        if (contentByte != null) {
+            try {
+                this.content = new String(contentByte, "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        this.contentByte = contentByte;
+    }
 
     @Transient
-	public List<ArticleComment> getComments() {
-		return comments;
-	}
+    public String getContent() {
+        if (contentByte != null) {
+            try {
+                return new String(contentByte, "utf-8");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return content;
+    }
 
-	public void setComments(List<ArticleComment> comments) {
-		this.comments = comments;
-	}
+    public void setContent(String content) {
+        contentByte = content.getBytes();
+    }
 
-	public String getPeriodical() {
-		return periodical;
-	}
+    public String getEditionPeriod() {
+        return editionPeriod;
+    }
 
-	public void setPeriodical(String periodical) {
-		this.periodical = periodical;
-	}
+    public void setEditionPeriod(String editionPeriod) {
+        this.editionPeriod = editionPeriod;
+    }
+
+    public String getMainImage() {
+        return mainImage;
+    }
+
+    public void setMainImage(String mainImage) {
+        this.mainImage = mainImage;
+    }
+
+    @Transient
+    public List<ArticleComment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<ArticleComment> comments) {
+        this.comments = comments;
+    }
+
+    public String getPeriodical() {
+        return periodical;
+    }
+
+    public void setPeriodical(String periodical) {
+        this.periodical = periodical;
+    }
 }
