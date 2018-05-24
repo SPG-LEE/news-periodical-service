c3987793ca5776e375a0f013f4bea7c0a900c805
mrlijianlong <517989745@qq.com>
2018-05-24 22:19:19 +0800

    热区保存修改

--- src/main/java/sq/news/periodical/entity/HotZone.java
+++ src/main/java/sq/news/periodical/entity/HotZone.java
@@ -19,8 +19,8 @@ import java.util.List;
 
 /**
  * 热区
- * 
- * 
+ * <p>
+ * <p>
  * {name:"",loginName:"",lastLoginDate:""}
  */
 @ApiModel(value = "热区")
@@ -31,57 +31,79 @@ import java.util.List;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE)
 public class HotZone extends BaseEntity {
 
-	private static final long serialVersionUID = 1L;
-	public static final String NAME = "热区";
-	@ApiModelProperty(value = "热区标题")
-	@JsonProperty
-	private String title;
-	@ApiModelProperty(value = "热区坐标")
-	@JsonProperty
-	private String mapInfo;
-	@ApiModelProperty(value = "文章Id")
-	@JsonProperty
-	private long articleId;
-	@ApiModelProperty(value = "版次Id")
-	@JsonProperty
-	private long editionId;
+    private static final long serialVersionUID = 1L;
+    public static final String NAME = "热区";
+    @ApiModelProperty(value = "热区标题")
+    @JsonProperty
+    private String title;
+    @ApiModelProperty(value = "热区坐标")
+    @JsonProperty
+    private String mapInfo;
+    @ApiModelProperty(value = "文章Id")
+    @JsonProperty
+    private long articleId;
+    @ApiModelProperty(value = "文章标题")
+    @JsonProperty
+    private String articleName;
+    @ApiModelProperty(value = "版次Id")
+    @JsonProperty
+    private long editionId;
 
-	public String getTitle() {
-		return title;
-	}
+    public String getArticleName() {
+        return articleName;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setArticleName(String articleName) {
+        this.articleName = articleName;
+    }
 
-	public String getMapInfo() {
-		return mapInfo;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setMapInfo(String mapInfo) {
-		this.mapInfo = mapInfo;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public long getArticleId() {
-		return articleId;
-	}
+    public String getMapInfo() {
+        return mapInfo;
+    }
 
-	public void setArticleId(long articleId) {
-		this.articleId = articleId;
-	}
+    public void setMapInfo(String mapInfo) {
+        this.mapInfo = mapInfo;
+    }
 
-	public long getEditionId() {
-		return editionId;
-	}
+    public long getArticleId() {
+        return articleId;
+    }
 
-	public void setEditionId(long editionId) {
-		this.editionId = editionId;
-	}
+    public void setArticleId(long articleId) {
+        this.articleId = articleId;
+    }
 
-	public void copyFrom(HotZone hotZone) {
-		this.articleId = hotZone.getArticleId();
-		this.editionId = hotZone.getEditionId();
-		this.mapInfo = hotZone.getMapInfo();
-		this.title = hotZone.getTitle();
-	}
+    public long getEditionId() {
+        return editionId;
+    }
+
+    public void setEditionId(long editionId) {
+        this.editionId = editionId;
+    }
+
+    public void copyFrom(HotZone hotZone) {
+        if (hotZone.getArticleId() != 0) {
+            this.articleId = hotZone.getArticleId();
+        }
+        if (hotZone.getEditionId() != 0) {
+            this.editionId = hotZone.getEditionId();
+        }
+        if (hotZone.getMapInfo() != null) {
+            this.mapInfo = hotZone.getMapInfo();
+        }
+        if (hotZone.getTitle() != null) {
+            this.title = hotZone.getTitle();
+        }
+        if (hotZone.getArticleName() != null) {
+            this.articleName = hotZone.getArticleName();
+        }
+    }
 }
